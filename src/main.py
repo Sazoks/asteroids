fa7493f36a8450442f70f6aa9b21926bc867cf60
Alexey Bobrov <bobrov.bobrov-alex@yandex.ru>
2022-07-01 13:46:09 +0700

    Начал рефакторинг генератора астероидов

--- src/main.py
+++ src/main.py
@@ -80,7 +80,7 @@ def main():
         skin=settings.player_skin,
         bullet_skin=settings.bullet_skin,
         health=200, speed=2.7, damage=22,
-        radius=25, shoot_delay=350, score=10000,
+        radius=25, shoot_delay=420, score=10000,
     )
     game_objects.players_group.add(player)
     # Регистрируем игрока в менджере активных усилений.
